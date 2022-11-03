0e72c5b2a3820babc74be783e038d5724b098b99
nickkept <nickit1899@gmail.com>
2022-11-03 16:16:43 +0100

    few changes made

--- js/mail.js
+++ js/mail.js
@@ -1,20 +1,25 @@
 // Creo array mail 
-const elencoMail = [ "topogigio@gmail.com", "nickit1899@gmail.com", "esempio@gmail.com", "fakemail@gmail.com" ];
+const elencoMail = ["topogigio@gmail.com", "nickit1899@gmail.com", "esempio@gmail.com", "fakemail@gmail.com"];
 // controllo se riesco a ricavarlo nella console 
+
 console.log(elencoMail);
 
+const userMail = prompt(`Inserisci mail`)
 let found = false;
 // avvio ciclo for 
 
-for ( let i = 0; i < elencoMail.length; i++ ) {
-    
-    if (elencoMail === `nickit1899@gmail.com`) {
+for (let i = 0; i < elencoMail.length; i++) {
+    const elementoElenco = elencoMail[i];
+
+
+    if (elementoElenco === `nickit1899@gmail.com`) {
         found = true;
-        console.log(elencoMail[i]);
     }
 }
 
 if (found === true) {
-    console.log( variabileElenco );
-    alert( `Hai inserito la mail giusta`);
+
+    alert(`Hai inserito la mail giusta`);
+} else {
+    alert(`Hai inserito la mail sbagliata`)
 }
