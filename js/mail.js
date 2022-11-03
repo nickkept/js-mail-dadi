6735407f5af795bc0ac8bb0c2efd3a8bd232e2c5
nickkept <nickit1899@gmail.com>
2022-11-03 16:19:04 +0100

    finished mail part

--- js/mail.js
+++ js/mail.js
@@ -4,15 +4,15 @@ const elencoMail = ["topogigio@gmail.com", "nickit1899@gmail.com", "esempio@gmai
 
 console.log(elencoMail);
 
-const userMail = prompt(`Inserisci mail`)
 let found = false;
+const userMail = prompt(`Inserisci mail`)
 // avvio ciclo for 
 
 for (let i = 0; i < elencoMail.length; i++) {
-    const elementoElenco = elencoMail[i];
+    
 
 
-    if (elementoElenco === `nickit1899@gmail.com`) {
+    if (userMail === `nickit1899@gmail.com`) {
         found = true;
     }
 }
